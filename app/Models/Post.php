6423f653ded75cb2f4753acf4a8492f2521632f6
Francisco Montenegro <franciscomontenegro22@gmail.com>
2022-05-06 09:07:44 -0300

    added laravel debugger toolbar

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['title'];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users',[UserController::class, 'index'])->name('users.index');
+
+// Route::get('users','UserController@index')->name('users.index');
